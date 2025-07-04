fb6b4a1db609ea9583ca01630ed26641df998fb4
Rahulrj777 <rahulsrinivasannkl@gmail.com>
2025-07-04 07:20:46 +0530

    Force redeploy with vercel.json update

--- src/pages/ShopingPage.jsx
+++ src/pages/ShopingPage.jsx
@@ -49,7 +49,7 @@ const ShopingPage = () => {
   };
 
   return (
-    <div className="max-w-6xl mx-auto p-6">
+    <div className="max-w-6xl mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6 text-center">Shop Products</h1>
 
       {/* Filters */}
