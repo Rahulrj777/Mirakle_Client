b21e447f8223153d3aff09e29c726d6f7a7a522c
Rahulrj777 <rahulsrinivasannkl@gmail.com>
2025-07-04 07:21:15 +0530

    Force redeploy with vercel.json update

--- src/pages/ShopingPage.jsx
+++ src/pages/ShopingPage.jsx
@@ -50,7 +50,7 @@ const ShopingPage = () => {
 
   return (
     <div className="max-w-6xl mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6 text-center">Shop Products</h1>
+      <h1 className="text-3xl font-bold mb-5 text-center">Shop Products</h1>
 
       {/* Filters */}
       <div className="flex justify-between items-center my-10 flex-wrap gap-4 w-full">
