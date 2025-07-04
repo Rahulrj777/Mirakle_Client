0b79bda79013273b3f3c6b416147886cf8236f87
Rahulrj777 <rahulsrinivasannkl@gmail.com>
2025-07-04 18:47:46 +0530

    Force redeploy with vercel.json update

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,7 +3,7 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import cartReducer from './cartSlice';
 import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk'; // ✅ Correct for ESM
 
 const persistConfig = {
   key: 'root', // not just 'cart'
