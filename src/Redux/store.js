144827bbac98391550efacbb05d396662b41161a
Rahulrj777 <rahulsrinivasannkl@gmail.com>
2025-07-04 18:05:35 +0530

    Force redeploy with vercel.json update

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,30 @@
 // src/redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './cartSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import { combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 
-export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
+// Configuration
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+// Combine reducers (useful if you add more in future)
+const rootReducer = combineReducers({
+  cart: cartReducer,
 });
+
+// Wrap with persistReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk],
+});
+
+// Persistor for React setup
+export const persistor = persistStore(store);
