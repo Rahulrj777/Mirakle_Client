f7ab95508cfd73cdc68dc7430f89e0e100426532
Rahulrj777 <rahulsrinivasannkl@gmail.com>
2025-07-05 11:41:28 +0530

    Force redeploy with vercel.json update

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import axios from 'axios';
-import Routing from '../src/Routing/Routing';
-import { API_BASE } from './utils/api';
+import Routing from '../src/Routing/Routing'
 
 const App = () => {
-  const cart = useSelector((state) => state.cart); // Get cart state from Redux
+  return (
+    <>
+      <Routing/>
+    </>
+  )
+}
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (!token) return;
-
-    const syncCart = async () => {
-      try {
-        await axios.post(`${API_BASE}/api/cart`, { items: cart }, {
-          headers: { Authorization: `Bearer ${token}` },
-        });
-      } catch (err) {
-        console.error('Cart sync failed:', err);
-      }
-    };
-
-    syncCart();
-  }, [cart]); // Sync whenever cart changes
-
-  return <Routing />;
-};
-
-export default App;
+export default App
