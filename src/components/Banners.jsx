17eff394cfb8cb66be04237ef01aac44917a1349
Rahulrj777 <rahulsrinivasannkl@gmail.com>
2025-07-14 17:38:24 +0530

    Force redeploy with vercel.json update

--- src/components/Banners.jsx
+++ src/components/Banners.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Banner from '../assets/banner.png';
 import BannerType1 from '../assets/bannerType1.png';
 import BannerType2 from '../assets/bannerType2.png';
-// import BannerType3 from '../assets/bannerType3.png';
+import BannerType3 from '../assets/bannerType3.png';
 
 const Banners = () => {
   return (
@@ -13,7 +13,7 @@ const Banners = () => {
       <div>
         <img src={BannerType1} alt="" />
         <img src={BannerType2} alt="" />
-        {/* <img src={BannerType3} alt="" /> */}
+        <img src={BannerType3} alt="" />
       </div>
     </div>
   )
