c453e530a6fd3b767f30a4053cde0b3c7979be04
Rahulrj777 <rahulsrinivasannkl@gmail.com>
2025-07-14 17:55:52 +0530

    Force redeploy with vercel.json update

--- src/components/Banners.jsx
+++ src/components/Banners.jsx
@@ -6,17 +6,26 @@ import BannerType3 from '../assets/bannerType3.jpg'
 
 const Banners = () => {
   return (
-    <div className="flex max-w-[1200px] mx-auto mt-6 gap-4">
-      {/* Main Banner */}
-      <div className="w-2/3">
-        <img src={Banner} alt="Main Banner" className="w-full h-full rounded-xl object-cover" />
+    <div className="flex gap-4 mt-28 px-6">
+      {/* Left side: Main big banner */}
+      <div className="w-[70%] h-[420px]">
+        <img
+          src={Banner}
+          alt="Main Banner"
+          className="w-full h-full object-cover rounded-xl shadow-md"
+        />
       </div>
 
-      {/* Side Banners */}
-      <div className="w-1/3 flex flex-col gap-4">
-        <img src={BannerType1} alt="Banner 1" className="w-full h-[33%] rounded-xl object-cover" />
-        <img src={BannerType2} alt="Banner 2" className="w-full h-[33%] rounded-xl object-cover" />
-        <img src={BannerType3} alt="Banner 3" className="w-full h-[33%] rounded-xl object-cover" />
+      {/* Right side: Smaller 3 stacked banners */}
+      <div className="w-[30%] flex flex-col gap-4">
+        {[BannerType1, BannerType2, BannerType3].map((img, i) => (
+          <img
+            key={i}
+            src={img}
+            alt={`Banner ${i + 1}`}
+            className="w-full h-[120px] object-cover rounded-xl shadow"
+          />
+        ))}
       </div>
     </div>
   )
