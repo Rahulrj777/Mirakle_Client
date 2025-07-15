4e0ea03bbfb73fbccefd41be7ddae8867d503011
Rahulrj777 <rahulsrinivasannkl@gmail.com>
2025-07-15 18:51:25 +0530

    Force redeploy with vercel.json update

--- src/components/Banners.jsx
+++ src/components/Banners.jsx
@@ -294,9 +294,9 @@ const Banners = () => {
 
   return (
     <div className="w-full h-full flex">
-      <div className="max-w-7xl mx-auto mt-6 px-4 flex gap-6 h-[510px]">
+      <div className="w-[80%] mx-auto mt-6 px-4 flex gap-6 h-[510px]">
         <div
-          className="w-[80%] h-full relative rounded-xl overflow-hidden"
+          className=" h-full relative rounded-xl overflow-hidden"
           onMouseEnter={() => setHovered(true)}
           onMouseLeave={() => setHovered(false)}
         >
