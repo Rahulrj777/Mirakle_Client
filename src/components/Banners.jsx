c24732ae86658a62aba7f169952f3c9d853d0a1f
Rahulrj777 <rahulsrinivasannkl@gmail.com>
2025-07-15 18:57:19 +0530

    Force redeploy with vercel.json update

--- src/components/Banners.jsx
+++ src/components/Banners.jsx
@@ -426,7 +426,7 @@ const Banners = () => {
         </div>
       </div>
       
-      <div className="w-[20%] h-full flex flex-col gap-4 min-h-0">
+      <div className="w-[20%] h-full flex flex-col gap-4 min-h-0 mt-6">
         {/* Search */}
         <div className="px-2">
           <input
