26b1b0e0d467f140060f1e87c470abe18b092883
Rahulrj777 <rahulsrinivasannkl@gmail.com>
2025-07-17 11:16:46 +0530

    Force redeploy with vercel.json update

--- src/components/ProductOffer.jsx
+++ src/components/ProductOffer.jsx
@@ -37,7 +37,7 @@ const OfferPage = () => {
       {/* <!-- Right Banner --> */}
       <div className="flex-1 bg-gray-100 rounded-xl p-6 flex flex-col justify-between items-start relative overflow-hidden">
         {/* Special Offer Badge */}
-        <div className="absolute top-0 left-0 w-[ 120px] h-[60px]">
+        <div className="absolute top-0 left-0 w-[ 160px] h-[80px]">
           <img src={specialoffer} alt="Special Offer" className="w-full h-full object-contain" />
         </div>
 
