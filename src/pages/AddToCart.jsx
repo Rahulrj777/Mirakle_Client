6809f6dcb46fe01c8db517c3d4365221c1fe1d03
Rahulrj777 <rahulsrinivasannkl@gmail.com>
2025-07-22 17:58:26 +0530

    Force redeploy with vercel.json update

--- src/pages/AddToCart.jsx
+++ src/pages/AddToCart.jsx
@@ -61,7 +61,7 @@ const AddToCart = () => {
                 onClick={() => setShowAddressModal(true)}
                 className="bg-blue-600 text-white px-4 py-2 rounded"
               >
-                Add Address
+                Select Address
               </button>
             )}
           </div>
